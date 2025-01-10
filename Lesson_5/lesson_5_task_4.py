5a1750b3cdb8d469ec51274abf1fca1e74a6686d
AntonSolovev82 <antonsolovev1982@gmail.com>
2025-01-10 21:27:04 +0300

    Учимся писать автоматизированные UI-тесты

---
+++ Lesson_5/lesson_5_task_4.py
@@ -0,0 +1,21 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+
+driver.get("http://the-internet.herokuapp.com/entry_ad")
+
+sleep(3)
+
+close_btn = driver.find_element(By.XPATH, "//div[@class='modal-footer']/p")
+
+close_btn.click()
+
+sleep(3)
+
+driver.quit()
